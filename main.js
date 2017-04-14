7710761651a89ee4c3f14bb9ecb5c33150523a1d
Shannon Rivers <shannon.rivers@gmail.com>
2017-04-14 13:13:11 -0600

    functional pixel art maker

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
+const palette = document.getElementById('palette');
+const container = document.getElementById('container');
+const current = document.getElementsByClassName('current')[0];
+
+
 for (let i=1; i<9441; i++){
     var pixel = document.createElement('div');
     pixel.className = 'pixel';
     container.appendChild(pixel);
 }
 
-container.addEventListener('click', function (event){
-  event.target.className = 'pixelRed';
+var pickedPaint = null;
+palette.addEventListener('click', function(event){
+  pickedPaint = event.target.className.slice(12);
+  console.log(pickedPaint);
+  current.className += ` ${pickedPaint}`;
 });
+
+container.addEventListener('click', function (event) {
+  event.target.className += ` ${pickedPaint}`;
+});
+
+
+// var red = document.getElementById('red');
+// red.addEventListener('click', function(event){
+//   pickedPaint = event.target.style.backgroundColor;
+//   console.log(pickedPaint);
+// });
+
+// orange.addEventListener('click', function(event){
+//   pickedPaint = event.target.style.backgroundColor;
+//   console.log(pickedPaint);
+// });
