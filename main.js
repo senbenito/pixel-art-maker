e6406fe0ae31aabb9ff57442d0ddaede687c734d
Shannon Rivers <shannon.rivers@gmail.com>
2017-04-17 13:31:01 -0600

    added in paintbrush functionality with Shift+Click - need toggle on/off indicator

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const palette = document.getElementById('palette');
 const container = document.getElementById('container');
 const current = document.getElementsByClassName('current')[0];
 
-
+//canvas maker expression
 for (let i=1; i<9441; i++){
     var pixel = document.createElement('div');
     pixel.className = 'pixel';
     container.appendChild(pixel);
 }
 
+//paint selector functionality
 var pickedPaint = null;
 palette.addEventListener('click', function(event){
   pickedPaint = event.target.className.slice(12);
@@ -16,10 +17,69 @@ palette.addEventListener('click', function(event){
   current.className += ` ${pickedPaint}`;
 });
 
-container.addEventListener('click', function (event) {
+//click to paint event & function
+// container.addEventListener('click', paintPixels);
+// container.addEventListener('mousedown', paintPixels);
+// container.addEventListener('mouseenter', paintPixels);
+// container.addEventListener('mouseup', paintPixels);
+//
+//
+function paintPixels() {
   event.target.className += ` ${pickedPaint}`;
-});
+}
+// container.addEventListener('mousedown', function(){
+  container.addEventListener('mousedown', paintStuff);
+  // container.addEventListener('mouseup', paintStuff);
 
+// });
+
+var shiftedTF = false;
+function paintStuff(event){
+  var shiftPress = event.shiftKey;
+  if (shiftPress && shiftedTF === false){
+    shiftedTF = true;
+  } else if (shiftPress && shiftedTF === true){
+    shiftedTF = false;
+  }
+
+  if (shiftedTF){
+    container.addEventListener('mouseover', paintPixels);
+    container.removeEventListener('click', paintPixels);
+  } else {
+    container.addEventListener('click', paintPixels);
+    container.removeEventListener('mouseover', paintPixels);
+  }
+}
+
+// function paintStuff(event){
+//   var shiftPress = event.shiftKey;
+//   var altPress = event.altKey;
+//
+//   if (shiftPress){
+//     container.addEventListener('mouseover', paintPixels);
+//     container.removeEventListener('click', paintPixels);
+//     console.log('shift pressed');
+//   } else if (altKey) {
+//     container.addEventListener('click', paintPixels);
+//     container.removeEventListener('mouseover', paintPixels);
+//     console.log('alt pressed');
+//   }
+// }
+
+
+// if mousedown, paint on mouseover.
+// else mouseup, no paint
+
+// function paintStuff(event){
+//   if (event.mousedown){
+//     container.addEventListener('mouseover', paintPixels);
+//   }
+// }
+
+//drag to paint event & function
+// container.addEventListener('mousedown', function (event) {
+//   event.target.className += ` ${pickedPaint}`;
+// });
 
 // var red = document.getElementById('red');
 // red.addEventListener('click', function(event){
